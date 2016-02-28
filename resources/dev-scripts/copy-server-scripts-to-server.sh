9cd4a4a5ddc120c2f7aca975cacf7f4e9a51ae1a
Brandon Okert <brandon.okert@hootsuite.com>
2016-02-27 22:24:07 -0800

    Updated script deploy script

--- resources/dev-scripts/copy-server-scripts-to-server.sh
+++ resources/dev-scripts/copy-server-scripts-to-server.sh
@@ -3,19 +3,16 @@
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../..
 SERVER_SCRIPTS_DIR=${ROOT_DIR}/resources/server-scripts
 
-echo "Docker host: $1"
-echo "SFTP Port: $2"
-echo "Docker Group Id: $3"
+echo "Docker host full address: $1"
+echo "Docker host secret key file: $2"
 
 if [ -z "$1" ]; then echo "Missing docker host address as first argument (format: user@123.432.41.11)"; exit 1; fi
-if [ -z "$2" ]; then echo "Missing SFTP port as second argument (format: 9911)";  exit 1; fi
-if [ -z "$3" ]; then echo "Missing Docker group id, which is needed to set permissions. Retrieve remotely via 'getent group docker | cut -d: -f3' (format: 991)";  exit 1; fi
+if [ -z "$2" ]; then echo "Missing secret key file (format: /path/to/key.pem)";  exit 1; fi
 
 echo "Copying files over to server..."
-sftp -P $2 $1 << EOF
+sftp -i $2 $1 << EOF
 
-cd /usr/local/bin
+cd /usr/local/bin/elophant
 put ${SERVER_SCRIPTS_DIR}/*.sh
-chgrp $3 *.sh
 
 EOF
